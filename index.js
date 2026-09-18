fd764b33f6da3909a8256d7c661c1ed7ff4461d6
Tushar Sharma <ts817479@gmail.com>
2026-09-18 19:01:21 +0530

    build

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/health", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello tushar!" });
+  res.status(200).json({ message: "Hello tushar sharma!" });
 });
 
 app.listen(PORT, () => {
